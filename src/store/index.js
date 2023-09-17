927b7f9e8a2a4622f11b980b91bd9f5761253b01
uvecon8 <volodimirsavin56@gmail.com>
2023-09-17 17:27:06 +0300

    Before deploy +6

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from "vuex";
 
 export default createStore({
     state: {
-        backendUrl: "http://45.94.158.125:8080/api/v1",
+        backendUrl: "http://theatreofplaywrightsapi.space:8080/api/v1",
         // backendUrl: "http://mysite.com:8000/api/v1",
 
     },
