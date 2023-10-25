ce9fb0270251af364b4e3f2b4d45eacdf87d8b34
uvecon8 <volodimirsavin56@gmail.com>
2023-10-25 18:11:51 +0300

    підписка

--- src/views/MainPageView.vue
+++ src/views/MainPageView.vue
@@ -6,7 +6,11 @@
     <div class="d_flex_column p_40px padding_faces">
       <div class="margin_both_auto">
         <div id="faces_7" class="d_grid">
-          <div v-for="playw in firstTwoRowsPlw" :key="playw.id" class="pad_1em padding_faces">
+          <div
+            v-for="playw in firstTwoRowsPlw"
+            :key="playw.id"
+            class="pad_1em padding_faces"
+          >
             <div class="c_pointer">
               <img
                 :src="playw.image"
@@ -27,7 +31,11 @@
           </div>
         </div>
         <div id="faces_6" class="d_grid_6">
-          <div v-for="playw in downRowPlw" :key="playw.id" class="pad_1em padding_faces">
+          <div
+            v-for="playw in downRowPlw"
+            :key="playw.id"
+            class="pad_1em padding_faces"
+          >
             <div class="c_pointer">
               <img
                 :src="playw.image"
@@ -51,6 +59,7 @@
       <div class="">
         <div id="idea_main" class="d_flex_column margin_both_auto">
           <div
+            id="n_idea"
             class="d_flex_row j_content_center upper_case open_sans name_dep t_left ptb_1em"
           >
             <div>ідея</div>
@@ -58,9 +67,15 @@
         </div>
         <div id="idea_main_list" class="d_grid_3 ptb_5em margin_both_auto">
           <div v-for="idea in ideas" :key="idea.id" class="pad_b1em">
-            <div v-html="idea.description" class="t_justify p_lr_1 padding_low_1000"></div>
-            <hr class="line" />
+            <div
+              v-html="idea.description"
+              class="t_justify p_lr_1 padding_low_1000 f_size_low_1000 open_sans"
+            ></div>
+            <!-- <hr class="line" /> -->
           </div>
+          <!-- <div class="upper_case f_weight_bold">
+            більше
+          </div> -->
         </div>
       </div>
 
@@ -74,35 +89,84 @@
         </div>
         <div id="plays_main_list" class="d_grid_3 ptb_5em">
           <div v-for="play in mainPlays" :key="play.id">
-            <div
-              class="w_22em h_35em margin_both_auto c_pointer overflow_hidden"
+            <router-link
+              class="pad_b1em"
+              :to="{
+                name: 'play',
+                params: {
+                  id: play.id_play,
+                  date_id: play.id,
+                  name: transcription(play.name),
+                },
+              }"
             >
-              <img
-                :src="repalcer(play.photo)"
-                :alt="play.name"
-                class="img_on_main h_100 w_100 zoom_hover"
-              />
-            </div>
-            <div class="p_abs_text w_max_content color_white">
-              <div class="f_oswald f_size_40">
-                {{ play.name }}
+              <div
+                class="w_22em h_35em margin_both_auto c_pointer overflow_hidden size_low_1000_img"
+              >
+                <img
+                  :src="repalcer(play.photo)"
+                  :alt="play.name"
+                  class="img_on_main h_100 w_100 zoom_hover"
+                />
               </div>
-              <div class="d_flex_row j_content_start f_source_sans">
-                <div>
-                  {{ play.dramaturg[0].first_name }}
-                  {{ play.dramaturg[0].last_name }}
+              <div
+                class="p_abs_text w_max_content color_white p_abs_text_low_1000"
+              >
+                <div class="f_oswald f_size_40">
+                  {{ play.name }}
+                </div>
+                <div class="d_flex_row j_content_start f_source_sans">
+                  <div>
+                    {{ play.dramaturg[0].first_name }}
+                    {{ play.dramaturg[0].last_name }}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div
-              class="p_abs_date w_max_content color_white open_sans f_size_32 f_weight_bold"
-            >
-              {{ checkDateToShow(play.id, play.on_play) }}
-            </div>
+              <div
+                class="p_abs_date w_max_content color_white open_sans f_size_32 f_weight_bold"
+              >
+                {{ checkDateToShow(play.id, play.on_play) }}
+              </div>
+            </router-link>
           </div>
         </div>
       </div>
-      <div></div>
+      <div id="subscribe" class="d_flex_row j_content_space_around w_70">
+        <div
+          id="lbl_subscribe"
+          class="d_flex_column open_sans upper_case t_left"
+        >
+          <div>
+            {{ subscribeLabel.text1 }}
+          </div>
+          <div class="f_weight_bold">
+            {{ subscribeLabel.text2 }}
+          </div>
+        </div>
+        <form
+          id="form_subscribe"
+          action="POST"
+          class="d_flex_row j_content_space_between w_50"
+        >
+          <div id="places_inputs" class="w_50 h_max horizontal_line">
+            <input
+              v-model="subscribe"
+              class="w_100 b_none f_oswald padding_4path sibscribe_input"
+              type="email"
+              name="email_subscribe"
+              id="e_subscribe"
+              placeholder="e-mail"
+            />
+          </div>
+          <div class="open_sans f_size_32 pad_low_1000">
+            <input
+              class="b_none bg_white_custom upper_case sibscribe_button"
+              type="button"
+              value="підписатись"
+            />
+          </div>
+        </form>
+      </div>
       <div></div>
     </div>
     <div>
@@ -127,7 +191,13 @@ export default {
       playwritersCaricatures: [],
       ideas: [],
       mainPlays: [],
-      subscribe: {},
+      subscribe: null,
+      subscribeLabel: {
+        text1: "хочу отримувати листи від",
+        text2: "театру драматургів",
+      },
+      showIdeas: false,
+      mobile: true,
     };
   },
   created() {
@@ -194,6 +264,94 @@ export default {
       }
       return;
     },
+    transcription(word) {
+      // Транскрипція з кирилиці на латиницю
+      let transcription_alpha = [
+        { value: ["а", "a"] },
+        { value: ["б", "b"] },
+        { value: ["в", "v"] },
+        { value: ["г", "g"] },
+        { value: ["ґ", "g"] },
+        { value: ["д", "d"] },
+        { value: ["е", "e"] },
+        { value: ["є", "ye"] },
+        { value: ["ж", "zh"] },
+        { value: ["з", "z"] },
+        { value: ["и", "y"] },
+        { value: ["і", "i"] },
+        { value: ["ї", "i"] },
+        { value: ["й", "y"] },
+        { value: ["к", "k"] },
+        { value: ["л", "l"] },
+        { value: ["м", "m"] },
+        { value: ["н", "n"] },
+        { value: ["о", "o"] },
+        { value: ["п", "p"] },
+        { value: ["р", "r"] },
+        { value: ["с", "s"] },
+        { value: ["т", "t"] },
+        { value: ["у", "u"] },
+        { value: ["ф", "f"] },
+        { value: ["х", "kh"] },
+        { value: ["ц", "ts"] },
+        { value: ["ч", "ch"] },
+        { value: ["ш", "sh"] },
+        { value: ["щ", "shch"] },
+        { value: ["ь", ""] },
+        { value: ["ю", "yu"] },
+        { value: ["я", "ya"] },
+        { value: ["0", "0"] },
+        { value: ["1", "1"] },
+        { value: ["2", "2"] },
+        { value: ["3", "3"] },
+        { value: ["4", "4"] },
+        { value: ["5", "5"] },
+        { value: ["6", "6"] },
+        { value: ["7", "7"] },
+        { value: ["8", "8"] },
+        { value: ["9", "9"] },
+        { value: [" ", "_"] },
+        { value: ["a", "a"] },
+        { value: ["b", "b"] },
+        { value: ["c", "c"] },
+        { value: ["d", "d"] },
+        { value: ["e", "e"] },
+        { value: ["f", "f"] },
+        { value: ["g", "g"] },
+        { value: ["h", "h"] },
+        { value: ["i", "i"] },
+        { value: ["j", "j"] },
+        { value: ["k", "k"] },
+        { value: ["l", "l"] },
+        { value: ["m", "m"] },
+        { value: ["n", "n"] },
+        { value: ["o", "o"] },
+        { value: ["p", "p"] },
+        { value: ["q", "q"] },
+        { value: ["r", "r"] },
+        { value: ["s", "s"] },
+        { value: ["t", "t"] },
+        { value: ["u", "u"] },
+        { value: ["v", "v"] },
+        { value: ["w", "w"] },
+        { value: ["x", "x"] },
+        { value: ["y", "y"] },
+        { value: ["z", "z"] },
+      ];
+      let list_new_word = [];
+      for (let x = 0; x < word.length; x++) {
+        for (let y = 0; y < transcription_alpha.length; y++) {
+          if (transcription_alpha[y].value[0] == word[x].toLowerCase()) {
+            list_new_word.push(transcription_alpha[y].value[1]);
+          }
+        }
+      }
+      return list_new_word.join("");
+    },
+    showMoreIdeas() {
+      // hide or show all ideas
+      this.showIdeas = !this.showIdeas;
+    },
   },
   computed: {
     firstTwoRowsPlw() {
@@ -212,12 +370,25 @@ export default {
         );
       });
     },
+    renderIdea() {
+      if (document.body.offsetWidth < 1000) {
+        if (!this.showIdeas) {
+          return this.ideas.filter((ideas) => {
+            return this.ideas.indexOf(ideas) == 0;
+          });
+        } else {
+          return this.ideas;
+        }
+      } else {
+        return this.ideas;
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
-@media screen and (max-width: 1210px) {
+@media screen and (max-width: 1500px) {
   #plays_main_list,
   #idea_main_list {
     grid-template-columns: repeat(2, 1fr);
@@ -234,16 +405,53 @@ export default {
   #faces_6 {
     padding-top: 5px;
   }
+  #subscribe,
+  #form_subscribe {
+    flex-direction: column;
+    width: 100%;
+  }
+  #form_subscribe {
+    width: 90%;
+    margin: 0 auto;
+  }
+  #places_inputs {
+    width: 100%;
+  }
+  #lbl_subscribe {
+    padding: 15px 10px;
+  }
+  #hider {
+    display: block;
+  }
+  #unpack {
+    display: block;
+  }
+  #n_idea {
+    padding-bottom: 0;
+  }
+
+  .size_low_1000_img {
+    height: 26em;
+  }
+
   .size_pictures_low_1000 {
     height: 50px !important;
     width: 40px !important;
-    
   }
-  .padding_faces{
+  .padding_faces {
     padding: 0 !important;
   }
   .padding_low_1000 {
     padding: 0.9em !important;
+  }
+  .pad_low_1000 {
+    padding: 15px 0;
+  }
+  .p_abs_text_low_1000 {
+    left: calc(100% / 20);
+  }
+  .f_size_low_1000 {
+    font-size: 0.8em;
   }
 }
 
@@ -272,6 +480,15 @@ export default {
 }
 
 .line {
+  display: none;
+}
+.sibscribe_input {
+  outline: none;
+}
+.sibscribe_button:hover {
+  font-weight: 600;
+}
+.unpack_ideas {
   display: none;
 }
 </style>
