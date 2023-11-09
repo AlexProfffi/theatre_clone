2d4e7cd72cc1c2f10b5339d24ca46b9bc0262571
uvecon8 <volodimirsavin56@gmail.com>
2023-11-09 15:44:03 +0200

    sort comment

--- src/components/comments/CommentsComponent.vue
+++ src/components/comments/CommentsComponent.vue
@@ -26,19 +26,18 @@
             </div>
             <div class="w_100">
               <textarea
-              class="b_radius_5 w_100"
-              placeholder="Напишіть відгук..."
-              name="comment"
-              id="txt"
-              
-              rows="5"
-              v-model="postComment.text"
-              @input="countSymbols()"
-            ></textarea>
+                class="b_radius_5 w_100"
+                placeholder="Напишіть відгук..."
+                name="comment"
+                id="txt"
+                rows="5"
+                v-model="postComment.text"
+                @input="countSymbols()"
+              ></textarea>
             </div>
             <div class="d_flex_row_reverse p_tb_5">
               <input
-              id="p_submit"
+                id="p_submit"
                 class="comment_b color_white f_oswald upper_case pad_03em b_radius_5"
                 type="submit"
                 value="Опублікувати"
@@ -51,29 +50,73 @@
         </div>
       </div>
     </div>
-    <div v-if="comments == null">
+    <div class="pad_b1em" v-if="comments == null || comments.length == 0">
       {{ noneComments }}
     </div>
-    {{ comments }}
-    <div
-      v-for="comment in comments"
-      :key="comment.id"
-      class="d_flex_column p_lr_2"
-    >
-      <div class="d_flex_row open_sans upper_case">
-        <div class="f_weight_bold pad_b1em">
-          {{ comment.who }}
+    <div class="d_flex_row">
+      <div
+        @click="reverseData = !reverseData"
+        class=" p_lr_2 "
+      >
+        <div class="c_pointer arr_sort">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="currentColor"
+            class="bi bi-sort-up"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"
+            />
+          </svg>
         </div>
       </div>
-      <div class="d_flex_row t_justify f_source_sans">
-        <div class="pad_b1em f_low_400">
-          {{ comment.text }}
+    </div>
+    <div v-if="!reverseData">
+      <div
+        v-for="comment in comments"
+        :key="comment.id"
+        class="d_flex_column p_lr_2"
+      >
+        <div class="d_flex_row open_sans upper_case">
+          <div class="f_weight_bold pad_b1em">
+            {{ comment.who }}
+          </div>
         </div>
+        <div class="d_flex_row t_justify f_source_sans">
+          <div class="pad_b1em f_low_400">
+            {{ comment.text }}
+          </div>
+        </div>
+        <div class="d_flex_row_reverse t_justify f_source_sans font_1 pad_b1em">
+          <DateComponent :dateTime="comment.date_create" :timeBool="true" />
+        </div>
+        <div class="horizontal_line"></div>
       </div>
-      <div class="d_flex_row_reverse t_justify f_source_sans font_1 pad_b1em">
-        <DateComponent :dateTime="comment.date_create" :timeBool="true" />
+    </div>
+    <div v-else>
+      <div
+        v-for="comment in reversedComments"
+        :key="comment.id"
+        class="d_flex_column p_lr_2"
+      >
+        <div class="d_flex_row open_sans upper_case">
+          <div class="f_weight_bold pad_b1em">
+            {{ comment.who }}
+          </div>
+        </div>
+        <div class="d_flex_row t_justify f_source_sans">
+          <div class="pad_b1em f_low_400">
+            {{ comment.text }}
+          </div>
+        </div>
+        <div class="d_flex_row_reverse t_justify f_source_sans font_1 pad_b1em">
+          <DateComponent :dateTime="comment.date_create" :timeBool="true" />
+        </div>
+        <div class="horizontal_line"></div>
       </div>
-      <div class="horizontal_line"></div>
     </div>
   </div>
 </template>
@@ -98,6 +141,7 @@ export default {
       token: localStorage.getItem("token"),
       userIn: localStorage.getItem("userInfo"),
       checkAuth: false,
+      reverseData: false,
       postComment: {
         text: null,
         who: null,
@@ -172,6 +216,11 @@ export default {
       }
     },
   },
+  computed: {
+    reversedComments: function () {
+      return [...this.comments].reverse();
+    },
+  },
 };
 </script>
   <style scoped>
@@ -213,5 +262,12 @@ export default {
   transform: scale(1.02);
   /* padding: 0.3em 0.5em; */
   box-shadow: none;
+}
+
+.arr_sort {
+  transition: all .3s ease-out;
+}
+.arr_sort:hover {
+  transform: rotate(0.5turn);
 }
 </style>
