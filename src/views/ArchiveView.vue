fecb77bc58970006ada481f86f3f5610003f82ef
uvecon8 <volodimirsavin56@gmail.com>
2023-11-16 16:29:13 +0200

    adapt to big screen11

--- src/views/ArchiveView.vue
+++ src/views/ArchiveView.vue
@@ -73,19 +73,17 @@
           <div class="w_30 d_flex_row j_content_space_around">
             <button
               :disabled="page == 1"
-              class="bg_white_custom b_none scale_hover"
+              class="bg_white_custom b_none scale_hover c_pointer"
               @click="goToPrev()"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="black"
-                class="bi bi-arrow-left-circle-fill"
-                viewBox="0 0 16 16"
+                width="15"
+                height="25"
+                viewBox="0 0 10.605 15.555"
               >
-                <path
-                  d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                <polygon
+                  points="10.605 12.727 5.656 7.776 10.605 2.828 7.777 0 0 7.776 7.777 15.555 10.605 12.727"
                 />
               </svg>
             </button>
@@ -94,19 +92,17 @@
             </span>
             <button
               :disabled="!showNext"
-              class="bg_white_custom b_none scale_hover"
+              class="bg_white_custom b_none scale_hover c_pointer"
               @click="goToNext()"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="black"
-                class="bi bi-arrow-right-circle-fill"
-                viewBox="0 0 16 16"
+                width="15"
+                height="25"
+                viewBox="0 0 10.605 15.555"
               >
-                <path
-                  d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                <polygon
+                  points="2.828 15.555 10.605 7.776 2.828 0 0 2.828 4.949 7.776 0 12.727 2.828 15.555"
                 />
               </svg>
             </button>
